5592d804003564e00b95ecad3fd2124c9eb827cf
KamranMdShaikh <kamranmdshaikh@gmail.com>
2025-03-01 22:04:08 +0600

    initial commit

---
+++ teplpate.py
@@ -0,0 +1,29 @@
+import os
+from pathlib import Path
+
+
+list_of_files = [
+    
+    ".github/worksflows",
+    f"src/__init__.py",
+    "src/components/__init__.py",
+    "src/components/data_ingestion.py",
+    "src/components/data_transformation.py",
+    "src/components/model_transformation.py",
+    "src/components/model_trainer.py",
+    "src/components/model_evaluation.py",
+    "src/components/model_deployment.py",
+    "src/pipeine/__init__.py",
+    "src/pipeine/training_pipeline.py",
+    "src/pipeine/prediction_pipeline.py",
+    "src/utils/utils.py",
+    "scr/unit/__init__.py",
+    "scr/integration/__init__.py",
+    "init_setup.sh",
+    "requirements.txt",
+    "requirements_dev.txt",
+    "pytest.ini",
+    
+    
+    
+]
